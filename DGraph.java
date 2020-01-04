b1ebe1e358d257d5bfad73b0a13a07dc77bbb42c
dolev1995 <57942180+dolev1995@users.noreply.github.com>
2020-01-04 23:25:32 +0200

    Add files via upload

---
+++ DGraph.java
@@ -0,0 +1,156 @@
+package dataStructure;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class DGraph implements graph
+{
+	public HashMap<Integer, node_data> nodesMap = new HashMap<Integer, node_data>();
+	public HashMap<Integer, HashMap<Integer,edge_data>> edgesMap = new HashMap<Integer, HashMap<Integer,edge_data>>();
+	public int edgesCounter=0;
+	public int nodeCounter=0;
+	public int MC = 0;
+
+	public DGraph()
+	{
+		this.nodesMap = new HashMap<Integer, node_data>();
+		this.edgesMap = new HashMap<Integer, HashMap<Integer,edge_data>>();
+		this.edgesCounter=0;
+		this.nodeCounter=0;
+		this.MC = 0;
+	}
+	public DGraph(graph G)
+	{
+		Collection<node_data> nodes= G.getV();
+		for (node_data b : nodes) {
+			node_data copynode= new node(b);
+			this.addNode(copynode);
+			Collection<edge_data> edegspernode= G.getE(b.getKey());
+			this.edgesMap.put(b.getKey(),new HashMap<Integer,edge_data>() );
+			if(edegspernode != null)
+			{
+				for (edge_data c : edegspernode) {////////////////////////////////////
+					edge_data copyedge= new edge(c) ;
+					this.edgesMap.get(b.getKey()).put(c.getDest(),copyedge);
+				}
+			}
+		}
+		this.MC = G.getMC();
+		this.edgesCounter=G.edgeSize();
+
+	}
+
+	@Override
+	public node_data getNode(int key) 
+	{
+		if (this.nodesMap.get(key)==null)
+			return null; 
+		return this.nodesMap.get(key); 
+	}
+
+	@Override
+	public edge_data getEdge(int src, int dest)
+	{
+		if (this.edgesMap.get(src).get(dest) != null)
+		{
+			return  (this.edgesMap.get(src).get(dest)); 
+		}
+		return null;
+	}
+
+	@Override
+	public void addNode(node_data n) 
+	{
+		int key = 	n.getKey();
+		this.nodesMap.put(key, n);
+		this.MC ++;
+		nodeCounter++;
+	}
+
+	@Override
+	public void connect(int src, int dest, double w)
+	{
+		if (this.nodesMap.get(src)==null || this.nodesMap.get(dest)== null)
+		{
+			System.out.println("eror");
+		}
+		else
+		{
+			edge newedge = new edge(src,dest,w);
+			if (this.edgesMap.get(src) == null) 
+			{
+				this.edgesMap.put(src, new HashMap<Integer,edge_data>());
+				this.edgesMap.get(src).put(dest, newedge);
+				edgesCounter++;
+				this.MC ++;
+			}
+			else
+			{
+				this.edgesMap.get(src).put(dest, newedge);
+				edgesCounter++;
+				this.MC ++;
+			}
+		}
+	}
+
+	@Override
+	public Collection<node_data> getV()
+	{
+		return this.nodesMap.values();
+	}
+
+	@Override
+	public Collection<edge_data> getE(int node_id) {
+		if (this.edgesMap.isEmpty()) { return null; }
+		if (this.edgesMap.get(node_id)==null) { return null; }
+		return this.edgesMap.get(node_id).values(); 
+	}
+
+	@Override
+	public node_data removeNode(int key)
+	{
+		if (this.nodesMap.containsKey(key))
+		{
+			node_data ans = this.nodesMap.remove(key);
+			int num = this.edgesMap.get(key).size();
+			this.edgesMap.remove(key);
+			this.nodeCounter = this.nodeCounter - num;
+			MC++;
+			return ans;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	@Override
+	public edge_data removeEdge(int src, int dest)
+	{
+		if (this.edgesMap.get(src).get(dest)==null) 
+			return null; 
+		edge_data edge = new edge((edge)this.edgesMap.get(src).get(dest));
+		this.edgesMap.get(src).remove(dest);
+		edgesCounter--;
+		this.MC++;
+		return edge;
+	}
+
+	@Override
+	public int nodeSize() {
+		return this.nodeCounter;
+	}
+
+	@Override
+	public int edgeSize() {
+		return	edgesCounter;
+	}
+
+	@Override
+	public int getMC()
+	{
+		return MC;
+	}
+
+}
